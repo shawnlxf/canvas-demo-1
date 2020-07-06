abbf350963ac2c6b537ec05f233abbe74e33c682
shawn <1838435618@qq.com>
2020-07-06 23:47:11 +0800

    update

--- main.js
+++ main.js
@@ -14,38 +14,59 @@ ctx.lineWidth = 5;
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let isTouchDevice = "ontouchstart" in document.documentElement;
 
 function draw(e) {
-  if (!isDrawing) {
-    return;
-  }
+    if (isTouchDevice) {
 
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+        x = e.touches[0].clientX;
+        y = e.touches[0].clientY;
 
-  [lastX, lastY] = [e.offsetX, e.offsetY];
+    } else {
+
+        if (!isDrawing) {
+            return;
+        }
+
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+    [lastX, lastY] = [x, y];
 }
 
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', (e) => {
-  isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-});
-canvas.addEventListener('mouseup', () => isDrawing = false );
-canvas.addEventListener('mouseout', () => isDrawing = false );
+if (isTouchDevice) {
+    canvas.ontouchstart = (e) => {
+        [lastX, lastY] = [e.touches[0].clientX, e.touches[0].clientY];
+    }
 
+    canvas.ontouchmove = (e) => {
+        draw(e);
+    }
+} else {
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mousedown', (e) => {
+        isDrawing = true;
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    });
+    canvas.addEventListener('mouseup', () => isDrawing = false);
+    canvas.addEventListener('mouseout', () => isDrawing = false);
+}
 
 function changeColor(e) {
-  ctx.strokeStyle = e.target.value;
-  //ctx.strokeStyle = this.value;
+    ctx.strokeStyle = e.target.value;
+    //ctx.strokeStyle = this.value;
 }
 
 colorPicker.addEventListener('change', changeColor);
 
 function clear() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 clearButton.addEventListener('click', clear);
